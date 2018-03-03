ef4a3bf7b4bd555334c67ee7b9ddf946264025b1
Ben <menubar@protonmail.com>
2018-03-02 19:45:31 -0500

    new

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
+const ip = require('./ip')
 const fetch = require('node-fetch')
-const parseString = require('xml2js').parseString;
+const parseString = require('xml2js').parseString
+const commands = require('./commands')
+const { schema } = require('./schema')
 
-const createUrl = (params) => {
-  var url = 'https://api.namecheap.com/xml.response?'
-
-  return url += Object.keys(params).map(key => {
-    return `${key}=${encodeURIComponent(params[key])}`
-  }).join('&')
-}
+const get = (obj, path) => path.reduce((acc, val) => 
+  acc[val] ? acc[val] : undefined, obj)
 
 function parse(xml) {
   return new Promise((resolve, reject) => {
@@ -17,39 +15,147 @@ function parse(xml) {
   })
 }
 
-function get(data, arr, def = null) {
-  return arr.reduce((acc, val) => acc && acc[val] ? acc[val] : def, data)
+
+const createUrl = (params) => {
+  let url = 'https://api.namecheap.com/xml.response?'
+
+  return url + Object.keys(params)
+    .map(key => {
+      return `${key}=${encodeURIComponent(params[key])}`
+    }).join('&')
 }
 
-function mapFields(json) {
-  return get(
-    json, [
-      'ApiResponse', 
-      'CommandResponse', 
-      0, 
-      'DomainCheckResult'
-    ]
-  ).map(x => x['$'])
+let ipAddress
 
-}
+class Namecheap {
+  constructor(config = {}) {
+    this.config = config
+  }
 
-function Namecheap (args) {
+  async request (params = {}) {
+    if (!this.ipAddress) {
+      this.ipAddress = await ip()
+    }
 
-  var params = Object.assign({}, {
-    Command: 'namecheap.domains.check'
-  }, args) 
+    let mergedParams = {
+      ClientIp: this.ipAddress,
+      ...params,
+      ...this.config
+    }
 
-  return function(domains) {
+    if (!mergedParams.UserName) {
+      mergedParams.UserName = mergedParams.ApiUser
+    }
     
-    params.DomainList = domains;
-
-    var url = createUrl(params)
-  
+    const url = createUrl(mergedParams)
     return fetch(url)
-    .then(resp => resp.text())
-    .then(text => parse(text))
-    .then(json => mapFields(json))
+      .then(resp => resp.text())
+      .then(text => parse(text))
+      .then(data => {
+        let response = data['ApiResponse']
+        if (response.Errors.length) {
+          return Promise.reject(response.Errors)
+        }
+        return response
+      })
+  }
+
+  merge(obj = {}) {
+    obj = Object.keys(obj).reduce((acc, val) =>
+      typeof obj[val] !== 'undefined'
+        ? (acc[val] = obj[val], acc)
+        : acc 
+    , {})
+    this.config = {
+      ...this.config,
+      ...obj
+    }
   }
 }
 
-module.exports = Namecheap
+class Domains extends Namecheap {
+  dns (SLD, TLD) {
+    this.merge({ SLD, TLD })
+    return this
+  }
+
+  ns(SLD, TLD, Nameserver) {
+    this.merge({ SLD,  TLD, Nameserver })
+    return this
+  }
+
+  transfer (TransferID) {
+    this.merge({ TransferID })
+    return this
+  }
+}
+
+class Users extends Namecheap {
+  address (AddressId) {
+    this.merge({ AddressId })
+    return this
+  }
+}
+
+class SSL extends Namecheap {}
+class Whoisguard extends Namecheap {}
+
+const classes = {
+  domains: Domains,
+  users: Users,
+  ssl: SSL,
+  whoisguard: Whoisguard
+}
+
+commands.forEach(command => {
+  let path = command.split('.')
+  let requiredParams = get(schema, path)
+  let base = classes[path.shift()]
+  
+  // function run (...args) {
+  //  let params = {}
+  //  this.config.Command = 'namecheap.' + command
+  //  if (
+  //    args.length && 
+  //    args.length === 1 &&
+  //    typeof args[0] === 'object'
+  //  ) {
+  //    params = args[0]
+  //  } else {
+  //    let names = template
+  //      .map(t => t.name.trim())
+  //      .filter(name => !this.config[name])
+  //
+  //    params = args.reduce((acc, arg, i) =>
+  //      (acc[names[i]] = arg, acc)
+  //    , {})
+  //  }
+
+  //  return this.request(params)
+  //}
+
+  function run(params = {}) {
+    requiredParams.forEach(param => {
+      if (!this.config[param.name] && !params[param.name]) {
+        throw new Error(param.name + ' is required')
+      }
+    })
+    this.config.Command = 'namecheap.' + command
+    return this.request(params)
+  }
+
+  if(path.length > 1) {
+  	let [methodName, subMethodName] = path
+    let method = base.prototype[methodName]
+    method[subMethodName] = run
+  } else {
+    base.prototype[path[0]] = run
+  }
+})
+
+module.exports = {
+  domains: config => new Domains(config),
+  users: config => new Users(config),
+  ssl: config => new SSL(config),
+  whoisguard: config => new Whoisguard(config)
+}
--- test/index.js
+++ test/index.js
@@ -2,8 +2,8 @@ const namecheap = require('../src/namecheap')
 
 namecheap.domains({
   ApiKey: process.env.NAMECHEAP_API_KEY,
-  UserName: 'brainprotips',
-  ApiUser: 'brainprotips',
+  UserName: process.env.NAMECHEAP_USER,
+  ApiUser: process.env.NAMECHEAP_USER,
   //Command: 'namecheap.domains.dns.setHosts',
 }).check({DomainList: 'lorem.com'})
 .then(resp => {
@@ -12,15 +12,3 @@ namecheap.domains({
 .catch(err => {
   console.log(JSON.stringify(err))
 })
-
-/*let cheap = new Namecheap({
-  ApiKey: process.env.NAMECHEAP_API_KEY,
-  UserName: 'brainprotips',
-  ApiUser: 'brainprotips',
-  Command: 'namecheap.domains.dns.setHosts',
-  SLD: 'cronjoy',
-  TLD: 'com',
-  HostName1: '_6465795ac8a947ae8c0477f974d6c5b9.cronjoy.com.',
-  RecordType1: 'CNAME',
-  Address1: '_3a394736fa3060e8d9c3ac9f8d784fed.acm-validations.aws.',
-})*/
